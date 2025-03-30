08e58e9273d4bf3ee9c08da339af837cefd7f57c
LINRAY14 <andrei.shunko1498@gmail.com>
2025-03-30 22:59:02 +0300

    Lesson_14_v4

--- src/TriangleArea.java
+++ src/TriangleArea.java
@@ -1,9 +1,13 @@
 public class TriangleArea {
+    public static void main(String[] args) {
+        double result = triangleArea(3, 4, 5);
+        System.out.println("Площадь треугольника: " + result);
+    }
+
     public static double triangleArea(double a, double b, double c) {
         if (a <= 0 || b <= 0 || c <= 0) {
             throw new IllegalArgumentException("Стороны треугольника должны быть положительными!");
         }
-
 
         if (a + b <= c || a + c <= b || b + c <= a) {
             throw new IllegalArgumentException("Треугольник с такими сторонами не существует!");
